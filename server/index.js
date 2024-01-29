ea486a679a4442c6112c4aae4edf36e3c3898288
Benjoseph Villamor <benjosephvillamor@gmail.com>
2024-01-29 15:28:14 -0800

    add listing working

--- server/index.js
+++ server/index.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //let's us access req.body
+
+//routes
+
+//create listing
+app.post("/listings", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newListing = await pool.query(
+      "INSERT INTO listings (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newListing.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get all listings
+app.get("/listings", async (req, res) => {
+  try {
+    const allListings = await pool.query("SELECT * FROM urbanfetch");
+    res.json(allListings.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(5000, () => {
     console.log("server has started on port 5000");
