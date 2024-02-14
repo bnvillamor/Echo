50bc00ea07108356ee446a450a65dfe2faaf0ec7
Benjoseph Villamor <benjosephvillamor@gmail.com>
2024-02-14 12:58:06 -0800

    firebase auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,41 @@
-import React, { Fragment } from "react";
-import './App.css';
+import Login from "./components/auth/login";
+import Register from "./components/auth/register";
 
-//components
+import Header from "./components/header";
+import Home from "./components/home";
+
+import { AuthProvider } from "./contexts/authContext";
+import { useRoutes } from "react-router-dom";
 import AddUser from "./components/AddUser";
 
 function App() {
+  const routesArray = [
+    {
+      path: "*",
+      element: <Login />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+  ];
+  let routesElement = useRoutes(routesArray);
   return (
-    <Fragment>
+    <AuthProvider>
+      <Header />
+      <div className="w-full h-screen flex flex-col">{routesElement}</div>
       <div className='container'>
         <AddUser />
       </div>
-    </Fragment>
+    </AuthProvider>
   );
 }
 
